1e8b090dbed52fdc7af4290463a20fb8bfec91ba
WindowsLaptop <tieu5230@colorado.edu>
2019-10-29 14:57:40 -0600

    add gitignore

--- face-rec.py
+++ face-rec.py
@@ -1,2 +1,10 @@
-import dlib
+import face_recognition
+import PIL.Image
+import PIL.ImageDraw
+
+# Load the image to get the faces 
+image = face_recognition.load_image_file("faces.png")
+
+#Get all faces
+fLocations = face_recognition.face_locations(image)
 
